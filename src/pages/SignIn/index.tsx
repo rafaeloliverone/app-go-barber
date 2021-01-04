6a8d66b7663a67dfbbe7ef9627747aca155171e4
Rafael Oliveira <rafaelbatistacg@gmail.com>
2021-01-04 19:10:14 -0300

    styling page SignIn and adding components Input and Button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Button, Image } from 'react-native';
+import { Image } from 'react-native';
 
-import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { Container, Title } from './styles';
 
 
 const SignIn: React.FC = ({ navigation }) => {
   return (
     <Container>
-      <Button title="SignUp" onPress={() => navigation.navigate('SignUp')}></Button>
+      {/* <Button title="SignUp" onPress={() => navigation.navigate('SignUp')}></Button> */}
       <Image source={logoImg}></Image>
       <Title>Faça seu login</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail"/>
+      <Input name="password" type="password" icon="lock" placeholder="Senha" secureText={true}/>
+      <Button onPress={() => {console.log('Entrando')}}>Entrar</Button>
     </Container>
   )
 }
